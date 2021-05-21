81d69bc215d72687883959c6467b26eb883eb43a
Tatiana Karmanova <karmanova.ts@yandex.ru>
2021-05-21 21:13:02 +0300

    NEW tasks

---
+++ 6dictionary.py
@@ -0,0 +1,5 @@
+weather = {"city": "Москва", 
+"temperature": "20"}
+print (weather.get("city"))
+weather ["temperature"] =  - 5
+print (weather)
---
+++ 6dictionary2.py
@@ -0,0 +1,6 @@
+weather = {"city": "Москва", 
+"temperature": "20"}
+print (weather.get("country"))
+country = ["Россия"]
+weather["date"]= "27.05.2019"
+print(len(weather))
